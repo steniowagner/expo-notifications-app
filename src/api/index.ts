a54de0d0668f67c9d1317a05f08fa5f44a7a889a
Stenio Wagner <stenio.wagner1@gmail.com>
2019-12-27 17:31:30 -0300

    add samples images

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
-const SERVER_BASE_URL = 'http://<YOUR_IP_HERE>:4000/expo-notifications/api/v1';
+const SERVER_BASE_URL = 'http://<YOUR IP HERE>:4000/expo-notifications/api/v1';
 
 export default SERVER_BASE_URL;
